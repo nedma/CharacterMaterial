a8ad8ac5746e77290a587cf88146f1cec9cb0214
nedma <realned_07@163.com>
2017-10-01 15:34:39 +0800

    * add

---
+++ UnityProject/Assets/Rotater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour 
+{
+    public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
